6497762d20c527300f21381cdf987c739d1cb9a2
Selina Chua <selina.a.chua@gmail.com>
2021-09-27 09:39:46 +1000

    Added tute code for wk3.

---
+++ wk03/six_middle_bits.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+/**
+ * We have 32 bits, and we want to get the middle 6 bits.
+ * 
+ */
+uint32_t six_middle_bits(uint32_t u) {
+    // TODO: complete this function.
+    return 0;
+}
