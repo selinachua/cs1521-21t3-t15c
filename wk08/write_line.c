dbed04b08682dbf7431c8dc8a0ecdea50dcd163a
Selina Chua <selina.a.chua@gmail.com>
2021-11-02 10:50:53 +1100

    Add todays' tute

---
+++ wk08/write_line.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+    // 0. Check that there are correct # of args.
+
+    // 1. Open the file.
+
+    // 2. Write input to file.
+
+    // 3. Close the file.
+    
+}
